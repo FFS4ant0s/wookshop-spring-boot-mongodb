19274a37b65cfa46a500b6b893794f827453524b
Fernando <silentguyband@gmail.com>
2024-10-03 11:52:09 -0300

    Adicionando comentarios aos posts

--- workshopmongodb/src/main/java/com/nelioalves/workshopmongo/domain/Post.java
+++ workshopmongodb/src/main/java/com/nelioalves/workshopmongo/domain/Post.java
@@ -1,18 +1,20 @@
 package com.nelioalves.workshopmongo.domain;
 
 import java.io.Serializable;
-import java.sql.Date;
-import java.util.Objects;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.nelioalves.workshopmongo.dto.AuthorDTO;
+import com.nelioalves.workshopmongo.dto.CommentDTO;
 
 @Document
-public class Post implements Serializable{
+public class Post implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	private String id;
 	private Date date;
@@ -20,12 +22,15 @@ public class Post implements Serializable{
 	private String body;
 	private AuthorDTO author;
 	
-	public Post() {}
+	private List<CommentDTO> comments = new ArrayList<>();
+	
+	public Post() {
+	}
 
-	public Post(String id, java.util.Date date2, String title, String body, AuthorDTO author) {
+	public Post(String id, Date date, String title, String body, AuthorDTO author) {
 		super();
 		this.id = id;
-		this.date = (Date) date;
+		this.date = date;
 		this.title = title;
 		this.body = body;
 		this.author = author;
@@ -70,10 +75,21 @@ public class Post implements Serializable{
 	public void setAuthor(AuthorDTO author) {
 		this.author = author;
 	}
-	
+
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
+	}
+
 	@Override
 	public int hashCode() {
-		return Objects.hash(id);
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
 	}
 
 	@Override
@@ -85,9 +101,11 @@ public class Post implements Serializable{
 		if (getClass() != obj.getClass())
 			return false;
 		Post other = (Post) obj;
-		return Objects.equals(id, other.id);
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
-
-	
-	
 }
